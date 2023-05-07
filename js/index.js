e4fb2ba35536d8a77cf24d3472f4b1512047c9ea
Jere <albarracinjeremias@gmail.com>
2023-05-07 20:04:08 -0300

    ingreso de datos

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let nombre = prompt('Ingresa el nombre completo')
+let ocupacion = prompt('Ingrse su ocupacion')
+
+let oracion = 'Me llamo ' + nombre + ' ' + 'y soy un ' + ocupacion
+
+alert(oracion)
